db057281ead39ee1dc0fa7891adb6f9dfe7cfdf9
rachmadep <rachmad.eko.p@mail.ugm.ac.id>
2018-04-17 22:22:49 +0700

    Make Auth && Database Configuration

---
+++ database/migrations/2018_04_17_150851_create_biodatas_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBiodatasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('biodatas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('nama');
+            $table->string('foto');
+            $table->string('telepon');
+            $table->string('website');
+            $table->string('alamat');
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('biodatas');
+    }
+}
